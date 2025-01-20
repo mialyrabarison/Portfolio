e751d5d57f752e4e8ca65b92233fb42f566bfa89
mialyrabarison <mialyrabarison@gmail.com>
2025-01-20 10:47:05 +0300

    initheader

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
-  <router-view></router-view>
+  <!-- Si on veut que toute les pages ont header et footer fixe, dia atsoina rehefa avy manao importation  -->
+  <HeaderComponent></HeaderComponent>
+  <router-view></router-view><!-- ETO NY CONTENUE AN PAGE selon router -->
 </template>
 
 <script>
+import HeaderComponent from './components/header/Header-component.vue';
+
 
 export default {
   name: 'App',
-  components: {
+  components: { HeaderComponent 
     
   }
 }
---
+++ src/components/header/Header-component.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="navbar">
+    <div class="logo">
+        <p> M. RABARISON</p>
+
+    </div>
+    <div class="menu">
+                <a href="">home</a>
+            
+                <a href="">About</a>
+           
+                <a href="">Skills</a>
+           
+                <a href="">Contact</a>
+    
+    </div>
+
+</div>
+    
+</template>
+<style src="./header.css"></style>
